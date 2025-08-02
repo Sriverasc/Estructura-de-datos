da954b4b928a63734519868efcc5fe6d93c02d8d
Sriverasc <sriverasc18000@gmail.com>
2025-08-02 01:51:59 -0600

    Fix: Bug de autoreferencia en la funcion enlistarListaDobleFinal

--- ListaDobleEnlazada.h
+++ ListaDobleEnlazada.h
@@ -7,7 +7,7 @@ struct ListaDobleEnlazada {
 };
 
 struct ListaDobleEnlazada* iniciarListaDobleEnlazada(enum Tipo tipo) {
-    struct ListaDobleEnlazada* lista = (ListaDobleEnlazada *)malloc(sizeof(ListaDobleEnlazada));
+    struct ListaDobleEnlazada* lista = (struct ListaDobleEnlazada *)malloc(sizeof(struct ListaDobleEnlazada));
     lista->frente = NULL;
     lista->final = NULL;
     lista->tipo = tipo;
@@ -17,27 +17,48 @@ struct ListaDobleEnlazada* iniciarListaDobleEnlazada(enum Tipo tipo) {
 
 int enlistarListaDobleInicio(struct ListaDobleEnlazada* lista, void* valor) {
     if (!lista) return -1;
-    
-    struct NodoD *nodo = (NodoD *)malloc(sizeof(NodoD *));
 
-    guardarValor(nodo, 2, lista->tipo, valor);
-    nodo->siguiente = lista->frente;
-    nodo->anterior = lista->final;
-    lista->frente = nodo;
-    lista->final->siguiente = nodo;
+    struct NodoD *nodo = (struct NodoD *)malloc(sizeof(struct NodoD));
+    if (!nodo) return -2;
+
+    guardarValor(nodo, 2, lista->tipo, valor); 
+
+    if (!lista->frente) { 
+        nodo->anterior = nodo;
+        nodo->siguiente = nodo;
+        lista->frente = nodo;
+        lista->final = nodo;
+    } else {
+        nodo->siguiente = lista->frente;
+        nodo->anterior = lista->final;
+
+        lista->frente->anterior = nodo;
+        lista->final->siguiente = nodo;
+
+        lista->frente = nodo;
+    }
 
     return 1;
 }
 
 int enlistarListaDobleFinal(struct ListaDobleEnlazada* lista, void* valor) {
     if (!lista) return -1;
+    if (!lista->final) {
+        enlistarListaDobleInicio(lista, valor);
+        return 1;
+    }
 
-    struct NodoD *nodo = (NodoD *)malloc(sizeof(NodoD *));
+    struct NodoD *nodo = (struct NodoD *)malloc(sizeof(struct NodoD *));
+    if(!nodo) return -2;
 
     guardarValor(nodo, 2, lista->tipo, valor);
+
     nodo->anterior = lista->final;
     nodo->siguiente = lista->frente;
+
+    lista->frente->anterior = nodo;
     lista->final->siguiente = nodo;
+
     lista->final = nodo;
 
     return 1;
