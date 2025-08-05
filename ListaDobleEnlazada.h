a6a22c78cd8afc3bb392ad0dce231a8341b1f94b
Sriverasc <sriverasc18000@gmail.com>
2025-08-05 11:48:21 -0600

    Feat: Reimplementacion de desenlistarListaDoble

--- ListaDobleEnlazada.h
+++ ListaDobleEnlazada.h
@@ -51,7 +51,7 @@ int enlistarListaDobleInicio(ListaDobleEnlazada* lista, void* valor) {
 }
 
 int enlistarListaDobleFinal(ListaDobleEnlazada* lista, void* valor) {
-    if (!lista->frente) return enlistarListaDobleInicio(lista, valor);
+    if (lista != NULL && !lista->frente) return enlistarListaDobleInicio(lista, valor);
     else if (!lista) return -1;
     else if (!valor) return -2;
 
@@ -78,7 +78,7 @@ int enlistarListaDobleFinal(ListaDobleEnlazada* lista, void* valor) {
 }
 
 int enlistarListaDobleEn(ListaDobleEnlazada* lista, void* valor, int posicion) {
-    if (!lista->frente || posicion == 0) return enlistarListaDobleInicio(lista, valor);
+    if (lista != NULL && (!lista->frente || posicion == 0)) return enlistarListaDobleInicio(lista, valor);
     else if (!lista) return -1;
     else if (!valor) return -2;
     else if (posicion < 0) return -3;
@@ -117,9 +117,93 @@ int enlistarListaDobleEn(ListaDobleEnlazada* lista, void* valor, int posicion) {
     return 1;
 }
 
+void* desenlistarListaDobleInicio(ListaDobleEnlazada* lista) {
+    if (!lista || !lista->frente) return NULL;
+
+    NodoD* nodo = lista->frente;
+    void* valor = nodo->dato;
+
+    if (lista->frente == lista->final) {
+        lista->frente = NULL;
+        lista->final = NULL;
+    } else {
+        lista->frente = nodo->siguiente;
+        lista->final->siguiente = lista->frente;
+        lista->frente->anterior = lista->final;
+    }
+
+    free(nodo);
+    return valor;
+}
+
+void* desenlistarListaDobleFinal(ListaDobleEnlazada* lista) {
+    if (!lista || !lista->frente) return NULL;
+
+    NodoD* nodo = lista->final;
+    void* valor = nodo->dato;
+
+    if (lista->frente == lista->final) {
+        lista->frente = NULL;
+        lista->final = NULL;
+    } else {
+        lista->final = nodo->anterior;
+        lista->final->siguiente = lista->frente;
+        lista->frente->anterior = lista->final;
+    }
+
+    free(nodo);
+    return valor;
+}
+
+void* desenlistarListaDobleEn(ListaDobleEnlazada* lista, int posicion) {
+    if (!lista || !lista->frente) return NULL;
+    if (posicion == 0) return desenlistarListaDobleInicio(lista);
+
+    int i = 0;
+    NodoD* nodo = lista->frente;
+
+    do {
+        if (i == posicion) {
+            if (lista->frente == lista->final) {
+                lista->frente = NULL;
+                lista->final = NULL;
+            } else {
+                NodoD* tempS = nodo->siguiente;
+                NodoD* tempA = nodo->anterior;
+
+                tempA->siguiente = tempS;
+                tempS->anterior = tempA;
+
+                if (nodo == lista->final) {
+                    lista->final = tempA;
+                }
+
+                if (nodo == lista->frente) {
+                    lista->frente = tempS;
+                }
+            }
+
+            void* valor = nodo->dato;
+            free(nodo);
+
+            return valor;
+        }
+        i++;
+        nodo = nodo->siguiente;
+    } while (nodo != lista->frente);
+
+    return NULL;
+}
+
 void imprimirListaDoble(ListaDobleEnlazada* lista) {
-    if (!lista) printf("Error: La lista no debe ser nula");
-    else if (!lista->frente) printf("La lista esta vacia");
+    if (!lista) {
+        printf("Error: La lista no debe ser nula");
+        return;
+    }
+    else if (!lista->frente) {
+        printf("La lista esta vacia");
+        return;
+    }
 
     NodoD* nodo = lista->frente;
 
@@ -127,4 +211,6 @@ void imprimirListaDoble(ListaDobleEnlazada* lista) {
         imprimirValor(nodo->dato, lista->tipo);
         nodo = nodo->siguiente;
     } while (nodo != lista->frente);
+
+    printf("\n");
 }
